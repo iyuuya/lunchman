3701466fcd7cc5e59162c1403f288274f7498c23
dnond <tarosuk@gmail.com>
2014-08-15 15:53:18 +0900

    fixed validation in Identity model

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,7 +1,6 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :uid, :provider
-  validates_uniqueness_of :uid, scope: :provider
+  validate :uid, presence: true, uniqueness: { scope: :provider }
 
   def self.find_for_oauth(auth)
     find_or_create_by(uid: auth.uid, provider: auth.provider)

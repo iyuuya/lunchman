1d7e3ed569e6eb1736ab9d6ade146f3148315500
dnond <tarosuk@gmail.com>
2014-08-15 18:46:09 +0900

    fixed login logic

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,16 +3,12 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    ActiveRecord::Base.transaction do
-      @user = User.find_for_oauth(request.env["omniauth.auth"], current_user )
 
-      if @user.persisted?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
-        sign_in_and_redirect @user, event: :authentication
-      else
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url #to Google etc
-      end
+    ActiveRecord::Base.transaction do
+      user = User.find_for_oauth(request.env["omniauth.auth"], current_user )
+
+      flash[:notice] = "ログイン成功！"
+      sign_in_and_redirect user, event: :authentication
     end
 
   end

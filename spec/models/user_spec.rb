eef7f0b3e6c987998e4ce7d32f0e8b477f3506a3
dnond <tarosuk@gmail.com>
2014-08-18 19:58:17 +0900

    make user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  service  = :google_oauth2
+  include_context "setup_OmniAuth_config", service
+
+  describe "relationships" do
+    it { should have_one :identity }
+  end
+
+  describe "find_or_create_with_email" do
+
+    context "when giving oauth user data " do
+
+      it "should user.name is oauth_user.info.name " do
+        user = User.find_or_create_with_email( oauth_user )
+        expect( user.name ).to eq( oauth_user.info.name )
+      end
+    end
+  end
+
+
+
+  describe "get_email_from_auth" do
+
+    context "when giving oauth user data " do
+
+      it "should email is oauth_user.info.email " do
+        email = User.get_email_from_auth( oauth_user )
+        expect( email ).to eq( oauth_user.info.email )
+      end
+    end
+  end
+
 end

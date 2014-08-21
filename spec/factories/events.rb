ec1993189727b0aa5a3b21cf1239503fafa96e6f
dnond <tarosuk@gmail.com>
2014-08-21 14:56:08 +0900

    fix event factory format

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :event do
-    name Faker::Name.title
-    leader_user_id 1
-    event_at { rand(1..30).days.from_now }
-    deadline_at { event_at - rand(1..30).hours }
-    comment "MyText"
+    name            { Faker::Name.title }
+    leader_user_id  { 1 }
+    event_at        { rand(1..30).days.from_now }
+    deadline_at     { event_at - rand(1..30).hours }
+    comment         { Faker::Lorem.paragraphs(1).first }
     max_paticipants { rand(3..30) }
-    venue_id 1
-    status 0
-    cancel_at nil
+    venue_id        { 1 }
+    status          { 0 }
+    cancel_at       { nil }
   end
 end

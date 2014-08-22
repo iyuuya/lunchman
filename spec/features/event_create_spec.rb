4bcb33018bd765b8d427477c0926fdd98e482a8b
dnond <tarosuk@gmail.com>
2014-08-22 13:02:25 +0900

    fix event feature spec

--- spec/features/event_create_spec.rb
+++ spec/features/event_create_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 describe "event_create" do
+  let(:event) { FactoryGirl.build(:event) }
+
+  include_context "setup_OmniAuth_config", :google_oauth2
+
+  before :each do
+    visit user_omniauth_authorize_path( provider: :google_oauth2 )
+
+    visit new_event_path
+
+    fill_in 'event[name]',             with: event.name
+
+    fill_in 'event[event_at_date]',    with: event.event_at.strftime("%Y年%m月%d日")
+    fill_in 'event[event_at_time]',    with: event.event_at.strftime("%H:%M %p")
+
+    fill_in 'event[deadline_at_date]', with: event.event_at.strftime("%Y年%m月%d日")
+    fill_in 'event[deadline_at_time]', with: event.event_at.strftime("%H:%M %p")
+
+    fill_in 'event[comment]',          with: event.comment
+    fill_in 'event[max_paticipants]',  with: event.max_paticipants
+  end
 
   context "fill_in form" do
-    include_context "login_and_fillin_form"
-
     it 'increse event count' do
       expect{ click_button I18n.t('layouts.event_new_label') }.to change( Event, :count ).by(1)
     end
-
   end
 
-
   context "fill_in form" do
-    include_context "login_and_fillin_form"
-
     before do
       click_button I18n.t('layouts.event_new_label')
     end
@@ -26,6 +40,5 @@ describe "event_create" do
     it 'no error classes in css' do
       expect( page ).not_to have_css('div.alert-danger')
     end
-
   end
 end

b231162c9123c4f1878915620262be23dbb897ec
dnond <tarosuk@gmail.com>
2014-08-26 16:46:13 +0900

    move DatabaseCleaner setting into support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,21 +16,6 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin

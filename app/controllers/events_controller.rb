48ac6e40cf374210cd10dda5169f7638d1da5bdd
dnond <tarosuk@gmail.com>
2014-08-27 11:53:55 +0900

    fix double quotation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,19 +17,19 @@ class EventsController < ApplicationController
     build_params = event_params
 
     build_params[:event_at] = Time.strptime(
-          "%s %s" % [format_date_string(build_params[:event_at_date]), build_params[:event_at_time]],
-          "%Y/%m/%d %H:%M %p")
+          '%s %s' % [format_date_string(build_params[:event_at_date]), build_params[:event_at_time]],
+          '%Y/%m/%d %H:%M %p')
 
     build_params[:deadline_at] = Time.strptime(
-          "%s %s" % [format_date_string(build_params[:deadline_at_date]), build_params[:deadline_at_time]],
-          "%Y/%m/%d %H:%M %p")
+          '%s %s' % [format_date_string(build_params[:deadline_at_date]), build_params[:deadline_at_time]],
+          '%Y/%m/%d %H:%M %p')
 
     build_params[:status] = Event.statuses[:normal]
 
     @event = current_user.event.build(build_params)
 
     if @event.save
-      redirect_to events_path, notice: I18n.t("layouts.notice.create_event")
+      redirect_to events_path, notice: I18n.t('layouts.notice.create_event')
     else
       @event.event_at_date = build_params[:event_at_date]
       @event.event_at_time = build_params[:event_at_time]

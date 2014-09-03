cf816e6b82118d38c2bb1e1402d5d1f4f7906e3a
dnond <tarosuk@gmail.com>
2014-09-03 10:44:50 +0900

    improve rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  rescue_from Exception,                        with: :render_500
-  rescue_from ActiveRecord::RecordNotFound,     with: :render_404
-  rescue_from ActionController::RoutingError,   with: :render_404
+  if !Rails.env.development?
+    rescue_from Exception,                        with: :render_500
+    rescue_from ActiveRecord::RecordNotFound,     with: :render_404
+    rescue_from ActionController::RoutingError,   with: :render_404
+  end
 
   def routing_error
     raise ActionController::RoutingError.new(params[:path])

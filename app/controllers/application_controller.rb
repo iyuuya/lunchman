8964af971b4dc6b4db4f65f8679ef27b80de3de0
dnond <tarosuk@gmail.com>
2014-09-03 14:57:52 +0900

    fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  if !Rails.env.development?
+  unless Rails.env.development?
     rescue_from Exception,                        with: :render_500
     rescue_from ActiveRecord::RecordNotFound,     with: :render_404
     rescue_from ActionController::RoutingError,   with: :render_404

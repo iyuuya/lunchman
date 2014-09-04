55c6fe093da5024a41fe5e95dc2775e2e10d760a
dnond <tarosuk@gmail.com>
2014-09-04 16:39:08 +0900

    fix event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name                { Faker::Name.title }
     leader_user_id      { 1 }
     event_at            { rand(1..30).days.from_now }
-    deadline_at         { event_at - rand(1..30).hours }
+    deadline_at         { event_at - rand(1..3).hours }
     comment             { Faker::Lorem.paragraphs(1).first }
     max_participants    { rand(3..30) }
     venue_id            { 1 }

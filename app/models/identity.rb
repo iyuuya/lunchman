dccb8467b11a9bf1563f52319dc4a4ea02c27f28
iyuuya <i.yuuya@gmail.com>
2016-01-23 02:19:59 +0900

    fix typo

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,4 +1,4 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
-  validate :uid, presence: true, uniqueness: { scope: :provider }
+  validates :uid, presence: true, uniqueness: { scope: :provider }
 end
